cf023eeedf259d5783dfa9d437c3fe222693e405
kwakuduah12 <115908616+kwakuduah12@users.noreply.github.com>
2024-06-25 14:44:39 +0000

    Queues and Stacks

--- Start/DataStructures/Stacks/stack_start.py
+++ Start/DataStructures/Stacks/stack_start.py
@@ -2,12 +2,17 @@
 # try out the Python stack functions
 
 # TODO: create a new empty stack
-
+stack = []
 
 # TODO: push items onto the stack
-
+stack.append(10)
+stack.append(11)
+stack.append(12)
+stack.append(13)
 
 # TODO: print the stack contents
-
+print(stack)
 
 # TODO: pop an item off the stack
+stack.pop()
+print(stack)
